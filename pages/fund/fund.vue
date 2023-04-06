dbb189a914c7cdd492a2a1f6c1ca40025ad947a5
vane <13525442@qq.com>
2023-04-06 19:56:40 +0800

    钱包与资金页

---
+++ pages/fund/fund.vue
@@ -0,0 +1,44 @@
+<template>
+	<gui-page :customHeader="true" :headerClass="['gui-bg-gray', 'gui-dark-bg-level-1']">
+		<!-- 自定义头部导航 -->
+		<template v-slot:gHeader>
+			<view style="height:44px;" class="gui-flex gui-nowrap gui-rows gui-align-items-center">
+				<gui-header-leading :home="false" :back="true"></gui-header-leading>
+				<view class="gui-flex1"></view>
+				<view class="gui-flex gui-row gui-justify-content-center">
+					<gui-switch-navigation
+						:width="300"
+						textAlign="center"
+						:isCenter="true"
+						activeDirection="center"
+						:items="[{ id: 0, name: '消费记录' }, { id: 1, name: '充值记录' }]"
+						@change="navchange"
+					></gui-switch-navigation>
+				</view>
+				<view style="width:220rpx;"></view>
+			</view>
+		</template>
+		<!-- 页面主体 -->
+		<template v-slot:gBody>
+			<view class="gui-padding">
+				<scroll-view :style="{ height: 100 + 'vh' }">
+					<view class="gui-m-b-20" v-for="n in 30" :key="n">
+						<view class="gui-flex gui-row gui-space-between">
+							<view class="gui-flex gui-column gui-flex1">
+								<text>充值300个懒人币</text>
+								<text class="gui-text-small  gui-text-black-opacity3">2023-03-01 09:04:15</text>
+							</view>
+							<view class="gui-flex gui-column">
+								<text class="gui-color-orange gui-bold">+30元</text>
+								<text class="gui-text-small gui-text-black-opacity3">交易关闭</text>
+							</view>
+						</view>
+						<view class="gui-border-b gui-m-t-20"></view>
+					</view>
+				</scroll-view>
+			</view>
+		</template>
+	</gui-page>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
---
+++ pages/wallet/wallet.vue
@@ -0,0 +1,87 @@
+<template>
+	<gui-page>
+		<template v-slot:gBody>
+				<view class="gui-bg-yellow gui-m-30 gui-p-30 gui-border-radius">
+					<text>账户余额</text>
+					<view class="gui-flex gui-space-between gui-align-items-center">
+						<view>
+							<text>￥</text>
+							<text class="gui-h1">0</text>
+						</view>
+						<view class="gui-bg-white gui-p-30 gui-border-radius"><text class="gui-color-orange gui-bold">充 值</text></view>
+					</view>
+				</view>
+
+				<view class="gui-m-30">
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title">
+								<text class="gui-list-title-text gui-list-one-line gui-primary-text">支付设置</text>
+								<text class="gui-list-title-desc gui-color-gray">开通会员自动续费更省钱</text>
+							</view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">消费记录</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">充值记录</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">听读券余额</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">使用兑换码兑换听读券</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">我的已购</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">我的拼团</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+
+					<navigator class="gui-list-items">
+						<view class="gui-list-body gui-border-b">
+							<view class="gui-list-title"><text class="gui-list-title-text gui-list-one-line gui-primary-text">我的赠送</text></view>
+						</view>
+						<text class="gui-list-arrow-right gui-icons gui-color-gray-light">&#xe601;</text>
+					</navigator>
+				</view>
+		</template>
+	</gui-page>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
