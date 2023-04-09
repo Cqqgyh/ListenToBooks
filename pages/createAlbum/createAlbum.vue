45d2366f78ae90cebee06b346f84ebfbceaa9d2a
vane <13525442@qq.com>
2023-04-09 16:24:24 +0800

    创建专辑页面小部分布局

---
+++ pages/createAlbum/createAlbum.vue
@@ -0,0 +1,60 @@
+<template>
+	<gui-page>
+		<!-- 页面主体 -->
+		<template v-slot:gBody>
+			<view class="gui-padding"><text class="gui-text gui-color-gray">创建专辑</text></view>
+			<view>
+				<form @submit="submit">
+					<view class="gui-bg-white gui-dark-bg-level-3 gui-padding-x">
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑名称</text>
+							<view class="gui-form-body"><input type="text" class="gui-form-input" name="name1" placeholder="请输入专辑名称" /></view>
+						</view>
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑分类</text>
+							<view class="gui-form-body">
+								这里应该使用popup选择
+							</view>
+						</view>
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑封面</text>
+							<view class="gui-form-body"><gui-upload-images ref="uploadimgcom" uploadServerUrl="https://您的域名/地址" :maxFileNumber="1"></gui-upload-images></view>
+						</view>
+
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑标签</text>
+							<view class="gui-form-body">
+								这里应该跳转到标签选择页
+							</view>
+						</view>
+						
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑卖点</text>
+							<view class="gui-form-body">
+								<input type="text" class="gui-form-input" name="name1" placeholder="请输入专辑卖点" />
+							</view>
+						</view>
+						<view class="gui-form-item gui-border-b">
+							<text class="gui-form-label">专辑简介</text>
+							<view class="gui-form-body">
+								<gui-editor ref="guieditor"></gui-editor>
+							</view>
+						</view>
+										
+
+					</view>
+					<view class="gui-margin-top gui-flex gui-row gui-nowrap gui-justify-content-end gui-padding-x">
+						<button type="default" class="gui-button gui-bg-primary gui-noborder" style="width:168rpx;" formType="submit">
+							<text class="gui-color-white gui-button-text">提交</text>
+						</button>
+					</view>
+					<view style="height:60rpx;"></view>
+				</form>
+			</view>
+		</template>
+	</gui-page>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+<style scoped></style>
