9759cfed75650b7d1a85504b3bf1b2e437936292
cqq <945327638@qq.com>
2023-04-12 20:28:15 +0800

    feat: 增加初始页面模版

---
+++ pages/template/template.vue
@@ -0,0 +1,19 @@
+<template>
+  <gui-page>
+    <template v-slot:gBody>
+      
+    </template>
+  </gui-page>
+</template>
+<script setup lang="ts">
+import { ref } from "vue"
+/* 响应式数据 */
+// 分类导航
+
+/* 方法 */
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
