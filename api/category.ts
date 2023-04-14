77404bbc37099f5c826abacff1c445176df05511
cqq <945327638@qq.com>
2023-04-14 09:27:45 +0800

    opt: 增加接口

--- api/category.ts
+++ api/category.ts
@@ -1,9 +1,23 @@
 import Service from '../utils/request'
+import { RequestResponse } from "../utils/interface"
+import { AttributeListInterface, CategoryListInterface } from "../pages/createAlbum/interfaces"
 
 class CateGory extends Service {
   findAllCategory1() {
     return this.get({
       url: '/api/album/category/findAllCategory1'
+    })
+  }
+  // 获取所有分类信息
+  findAllCategory(): Promise<RequestResponse<CategoryListInterface[]>> {
+    return this.get({
+      url: '/api/album/category/getBaseCategoryList'
+    })
+  }
+  // 根据一级分类Id 查询分类属性数据
+  getAttrList(category1Id: string | number): Promise<RequestResponse<AttributeListInterface[]>> {
+    return this.get({
+      url: `/api/album/category/findAttribute/${category1Id}`
     })
   }
 }
