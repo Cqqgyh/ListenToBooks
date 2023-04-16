26766de1b14a009ddb59c034f6facb3c24734407
cqq <945327638@qq.com>
2023-04-16 14:35:31 +0800

    feat: 我的作品页面功能完善

---
+++ components/TrackItemCard/TrackItemCard.vue
@@ -0,0 +1,152 @@
+<template>
+  <uni-card>
+    <view class="gui-margin-top">
+      <view class="gui-card-body gui-border-b gui-flex gui-rows gui-nowrap">
+        <image
+          mode="aspectFill"
+          :src="data.coverUrl"
+          class="gui-card-img"></image>
+        <view class="gui-card-desc">
+          <view class="gui-flex gui-rows gui-nowrap gui-align-items-center">
+            <text
+              class="gui-card-name gui-primary-text gui-ellipsis">{{ data.trackTitle }}
+            </text>
+          </view>
+          <view class="gui-flex gui-rows gui-nowrap gui-align-items-center gui-m-t-30">
+            <view class="gui-m-r-20">
+              <uni-icons custom-prefix="iconfont" type="shengyin_o" class="gui-m-r-10"></uni-icons>
+              <text>{{ data.includeTrackCount}}</text>
+            </view>
+            <view class="gui-m-r-20">
+              <uni-icons custom-prefix="iconfont" type="erji" class="gui-m-r-10"></uni-icons>
+              <text>{{ data.playStatNum}}</text>
+            </view>
+            <view class="gui-m-r-20">
+              <uni-icons custom-prefix="iconfont" type="gouwuche3" class="gui-m-r-10"></uni-icons>
+              <text>{{ data.buyStatNum}}</text>
+            </view>
+            <view class="gui-m-r-20">
+              <uni-icons custom-prefix="iconfont" type="pinglun" class="gui-m-r-10"></uni-icons>
+              <text>{{ data.albumCommentStatNum}}</text>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="gui-card-footer gui-flex gui-rows gui-nowrap gui-space-between gui-align-items-center">
+        <text
+          @click="shareHandler()"
+          class="gui-icons gui-color-gray gui-card-footer-item gui-border-r">&#xe615; 分享
+        </text>
+        <text
+          class="gui-icons gui-color-gray gui-card-footer-item gui-border-r">&#xe614;数据
+        </text>
+        <text
+          @click="() => editItemHandler(data.albumId)"
+          class="gui-icons gui-color-gray gui-card-footer-item gui-border-r">&#xe69e; 编辑
+        </text>
+        <text
+          @click="deletePopUpRef.open()"
+          class="gui-icons gui-color-gray gui-card-footer-item">&#xe794; 删除
+        </text>
+      </view>
+    </view>
+  </uni-card>
+  <!--  分享弹窗-->
+  <uni-popup ref="shareRef" type="share" safeArea backgroundColor="#fff">
+    <uni-popup-share></uni-popup-share>
+  </uni-popup>
+  <!--  删除弹窗-->
+  <!-- 提示窗示例 -->
+  <uni-popup ref="deletePopUpRef" type="dialog">
+    <uni-popup-dialog
+      type="error"
+      cancelText="取消"
+      confirmText="确认"
+      title="注意"
+      content="你确定要删除吗？"
+      @confirm="() => deleteTrackInfo(data.trackId)"
+    ></uni-popup-dialog>
+  </uni-popup>
+</template>
+<script setup lang="ts">
+import { PropType, ref } from "vue"
+import { AlbumInfoInterface } from "../../api/albums/interfaces"
+import { albumsService } from "../../api"
+import UniPopup from "../../uni_modules/uni-popup/components/uni-popup/uni-popup.vue"
+/* 响应式数据 */
+const shareRef = ref<InstanceType<typeof UniPopup>>(null)
+const deletePopUpRef = ref<InstanceType<typeof UniPopup>>(null)
+const props = defineProps({
+  data: {
+    type: Object as PropType<AlbumInfoInterface>,
+    required: true,
+    default: () => {}
+  },
+  editItemHandler: {
+    type: Function,
+    required: true,
+    default: () => {}
+  },
+  deleteItemHandler: {
+    type: Function,
+    required: true,
+    default: () => {}
+  }
+})
+// 分类导航
+
+/* 方法 */
+// 删除专辑信息请求
+  const deleteTrackInfo = async (id: number) => {
+  try {
+    const res = await albumsService.deleteTrackInfo(id)
+    console.log(res)
+    // 删除成功 通知父组件父组件在数据列表中删除此项
+    props.deleteItemHandler(id)
+  } catch (error) {
+    console.log(error)
+  }
+}
+// 分享
+const shareHandler = () => {
+  shareRef.value.open()
+}
+/* 声明周期 */
+</script>
+
+<style lang="scss">
+/* 卡片视图 */
+.gui-card-body{
+  padding-bottom:10rpx;
+}
+.gui-card-img{
+  width:120rpx;
+  height:120rpx;
+  border-radius:80rpx;
+}
+.gui-card-desc{
+  width:400rpx;
+  margin-left:25rpx;
+  flex:1;
+}
+.gui-card-name{
+  font-size:28rpx;
+  line-height:40rpx;
+  margin-right:20rpx;
+}
+.gui-card-text{
+  line-height:40rpx;
+  font-size:24rpx;
+}
+.gui-card-footer{
+  margin-top:20rpx;
+}
+.gui-card-footer-item{
+  width:100rpx;
+  text-align:center;
+  flex:1;
+  line-height:38rpx;
+  font-size:26rpx;
+}
+
+</style>
--- pages/myWork/myWork.vue
+++ pages/myWork/myWork.vue
@@ -1,76 +1,211 @@
+
 <template>
-  <gui-page>
-    <template v-slot:gBody>
-      <view class="gui-flex1 gui-flex gui-column gui-padding">
-        <!-- 分类导航 -->
-        <view class="">
-          <gui-switch-navigation
-            textAlign="center"
-            :isCenter="true"
-            activeDirection="center"
-            :size="0"
-            :margin="0"
-            padding="30rpx"
-            activeLineHeight="4rpx"
-            activeLineWidth="40rpx"
-            :items="navItems"
-            @change="navchange"
-          ></gui-switch-navigation>
+  <view class="content">
+    <z-paging
+      ref="zPagingRef"
+      v-model="pageData[pageData.currentPageNav].list"
+      @query="getListInfo"
+      show-refresher-update-time
+      auto-show-back-to-top>
+      <!-- 需要固定在顶部不滚动的view放在slot="top"的view中，如果需要跟着滚动，则不要设置slot="top" -->
+      <template #top>
+        <view class="gui-dark-bg-level-3 gui-padding nav-top-container">
+          <!-- 分类导航 -->
+          <view class="">
+            <gui-switch-navigation
+              textAlign="center"
+              :isCenter="true"
+              activeDirection="center"
+              :size="0"
+              :margin="0"
+              padding="30rpx"
+              activeLineHeight="4rpx"
+              activeLineWidth="40rpx"
+              :items="navItems"
+              @change="(index)=>navChange(index,navItems[index].id)"
+            ></gui-switch-navigation>
+          </view>
+          <!-- 下拉选择-->
+          <view class="select-container">
+            <gui-select-menu ref="guiSelectMenuRef" :items="selectItemNames" @select="select"></gui-select-menu>
+            <view class="gui-text-small select-add" @click="()=>addOrEditItem()">
+              <text class="gui-icons gui-block gui-color-gray gui-text">&#xe6c7;</text>
+            </view>
+          </view>
         </view>
-        <!-- 下拉选择-->
-        <view class="select-container">
-          <gui-select-menu :items="selectItemNames" @select="select"></gui-select-menu>
-		  <view class="gui-text-small select-add">
-			<text class="gui-icons gui-block gui-color-gray gui-text">&#xe6c7;</text>
-		  </view>
-        </view>
-      </view>
-    </template>
-  </gui-page>
+      </template>
+      <!-- 如果希望其他view跟着页面滚动，可以放在z-paging标签内 -->
+      <!--			专辑列表Item-->
+      <AlbumItemCard
+        v-if="pageData.currentPageNav === 'albumListInfo'"
+        v-for="(item,index) in pageData.albumListInfo.list"
+        :data="item"
+        :editItemHandler="addOrEditItem"
+        :deleteItemHandler="deleteItem"
+        :key="item.albumId"></AlbumItemCard>
+      <!--			声音列表Item-->
+      <TrackItemCard
+        v-if="pageData.currentPageNav === 'trackInfoListInfo'"
+        v-for="(item,index) in pageData.trackInfoListInfo.list"
+        :data="item"
+        :editItemHandler="addOrEditItem"
+        :key="item.albumId"></TrackItemCard>
+    </z-paging>
+  </view>
 </template>
+
 <script setup lang="ts">
-import { ref } from "vue"
+import { reactive, ref } from "vue"
+import ZPaging from "../../uni_modules/z-paging/components/z-paging/z-paging.vue"
+import { albumsService } from "../../api"
+import GuiSelectMenu from "../../Grace6/components/gui-select-menu.vue"
+import { AlbumInfoListInterface, TrackInfoListInterface } from "../../api/albums/interfaces"
 /* 响应式数据 */
+const zPagingRef = ref<InstanceType<typeof ZPaging>>()
+const guiSelectMenuRef = ref<InstanceType<typeof GuiSelectMenu>>()
 // 分类导航
 const navItems = ref([
-  { name: "专辑", id: 0 },
-  { name: "声音", id: 1 },
-  { name: "视频", id: 2 },
-  { name: "有声PPT", id: 3 },
-  { name: "咔嚓笔记", id: 4 },
+  { name: "专辑", id: 'albumListInfo' },
+  { name: "声音", id: 'trackInfoListInfo' },
+  { name: "视频", id: '2' },
+  { name: "有声PPT", id: '3' },
+  { name: "咔嚓笔记", id: '4' }
 ])
 // 下拉选择
 const selectItems = ref([
-  { name: "全部", id: 0 },
-  { name: "进行中", id: 1 },
-  { name: "已通过", id: 2 },
-  { name: "未通过", id: 3 }
+  { name: "全部", status: '0301' },
+  { name: "进行中", status: '0302' },
+  { name: "已通过", status: '0301' },
+  { name: "未通过", status: '0301' }
 ])
 // 计算下拉选择的名字数组集合
 const selectItemNames = ref(selectItems.value.map((item) => item.name))
+// 定义页面数据
+const pageData = reactive({
+  currentPageNav:'albumListInfo',
+  status: selectItems.value[0].status, // 状态
+  userId: 0,
+  // 专辑列表
+  albumListInfo: {
+    // 专辑列表
+    list: [],
+    // 专辑新增修改路由路径
+    addEditRoutePath: '/pages/createAlbum/createAlbum',
+    // getListInfo搜索参数
+    query: {
+      albumTitle: "",
+    },
+  },
+  // 声音列表
+  trackInfoListInfo: {
+    // 专辑列表
+    list: [],
+    // 声音新增修改路由路径
+    addEditRoutePath: '/pages/createAlbum/createAlbum',
+    // getListInfo搜索参数
+    query: {
+      trackTitle: "",
+      status: "", // 状态
+      userId: 0
+    },
+  }
+})
 
 /* 方法 */
+// 重置下拉列表数据
+const resetSelectItems = () => {
+  selectItems.value = [
+    { name: "全部", status: '0301' },
+    { name: "进行中", status: '0302' },
+    { name: "已通过", status: '0301' },
+    { name: "未通过", status: '0301' }
+  ]
+  // 重新选择为0
+  guiSelectMenuRef.value.setCurrentIndex(0)
+  // 重置状态信息
+  pageData.status = selectItems.value[0].status
+}
 // 导航切换
-const navchange = (item: number | string) => {
-  console.log(item)
+const navChange = (item: number | string,navItemId:string) => {
+  pageData.currentPageNav = navItemId
+  // 重置下拉列表数据
+  resetSelectItems()
+  // 当切换tab或搜索时请调用组件的reload方法
+  zPagingRef.value.reload()
+  console.log('navChange',item,navItemId)
+}
+// 获取列表数据
+const getListInfo = async (page: number, limit: number) => {
+  //组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
+  //这里的pageNo和pageSize会自动计算好，直接传给服务器即可
+  //模拟请求服务器获取分页数据，请替换成自己的网络请求
+  const params = {
+    page: page,
+    limit: limit
+  }
+  try {
+    if (pageData.currentPageNav === 'albumListInfo') {
+      // 专辑列表
+      const res = await albumsService.getAlbumList({ ...params,albumInfoQuery: { ...pageData.albumListInfo.query,status:pageData.status,userId:pageData.userId } })
+      //将请求的结果数组传递给z-paging
+      zPagingRef.value.complete(res.data.records);
+    } else if (pageData.currentPageNav === 'trackInfoListInfo') {
+      // 声音列表
+      const res = await albumsService.getTrackList({ ...params,trackInfoQuery: { ...pageData.trackInfoListInfo.query,status:pageData.status,userId:pageData.userId } })
+      //将请求的结果数组传递给z-paging
+      zPagingRef.value.complete(res.data.records);
+    }
+  } catch (error) {
+    console.log(error)
+    zPagingRef.value.complete(false);
+  }
+}
+// 子组件被删除，触发父组件的删除列表子项事件
+const deleteItem = (id: number | string) => {
+  if (pageData.currentPageNav === 'albumListInfo') {
+    // 专辑列表
+    pageData.albumListInfo.list = pageData.albumListInfo.list.filter((item:AlbumInfoListInterface) => item.albumId !== id)
+  } else if (pageData.currentPageNav === 'trackInfoListInfo') {
+    // 声音列表
+    pageData.trackInfoListInfo.list = pageData.trackInfoListInfo.list.filter((item:TrackInfoListInterface) => item.trackId !== id)
+  }
+}
+// 添加
+const addOrEditItem = (id?:number | string) => {
+  const idStr = id ? `?id=${id}` : ''
+  uni.navigateTo({
+    url: (pageData[pageData.currentPageNav as 'albumListInfo']).addEditRoutePath + idStr
+  })
 }
 // 下拉选择切换
-const select = (item: number | string) => {
-  console.log(item)
+const select = (index: number) => {
+  // 切换请求列表的状态信息
+  pageData.status = selectItems.value[index].status
+  // 当切换tab或搜索时请调用组件的reload方法
+  zPagingRef.value.reload()
 }
-// 去往创作专辑页面
+// 滑动中
 </script>
 
 <style lang="scss" scoped>
+.swiper{
+  height: 100%;
+}
+.nav-top-container {
+  background-color: #fff;
+  padding-bottom: 0;
+}
 .select-container {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  .select-add{
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 50rpx;
+
+  .select-add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50rpx;
   }
 }
 </style>
+
