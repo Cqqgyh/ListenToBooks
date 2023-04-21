d3de514fce78c8948ed1586c453e953d65be56f6
cqq <945327638@qq.com>
2023-04-21 08:39:51 +0800

    opt: 开发期间增加固定token

--- stores/user.ts
+++ stores/user.ts
@@ -4,7 +4,7 @@ export const useUserStore = defineStore('user', {
   state: () => {
     return {
       user: JSON.parse(uni.getStorageSync(USER_KEY) || '{}'),
-      token: uni.getStorageSync(TOKEN_KEY) || '',
+      token: uni.getStorageSync(TOKEN_KEY) || 'c2362386c59745c7bd4b9c43bb659f2a',
     }
   },
   actions: {
