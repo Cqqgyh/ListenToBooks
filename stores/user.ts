809abb05f7280b914f0ee35a33b91f926a0339f4
cqq <945327638@qq.com>
2023-04-23 16:33:28 +0800

    opt:增加类型限制

--- stores/user.ts
+++ stores/user.ts
@@ -1,13 +1,13 @@
 import { defineStore } from "pinia"
 import { TOKEN_KEY, USER_KEY } from "../utils/constant"
 import { user } from "../api"
-import { UpdateUserInfoInterface, WechatUserInfoInterface } from "../api/user/interfaces"
+import { UpdateUserInfoInterface, UserInfoInterface, WechatUserInfoInterface } from "../api/user/interfaces"
 import { clearRedirectUrl, getRedirectUrl } from "../utils/utils"
 
 export const useUserStore = defineStore("user", {
   state: () => {
     return {
-      user: JSON.parse(uni.getStorageSync(USER_KEY) || "{}"),
+      user: JSON.parse(uni.getStorageSync(USER_KEY) || "{}") as UserInfoInterface,
       token: uni.getStorageSync(TOKEN_KEY) || "c2362386c59745c7bd4b9c43bb659f2a"
     }
   },
