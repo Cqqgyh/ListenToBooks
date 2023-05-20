5a6ba960fb3b23c32169e08589008d74dc9c0a3d
cqq <945327638@qq.com>
2023-05-20 11:57:12 +0800

    feat: 增加webSocket基础路径

--- config/confjg.ts
+++ config/confjg.ts
@@ -2,3 +2,5 @@
 export const BASE_URL = "http://139.198.163.91:8500"
 // 基础上传路径
 export const BASE_UPLOAD_URL = "http://139.198.163.91:8500"
+// webSocket基础路径
+export const WebSocket_BASE_URL = "ws://139.198.163.91:8507/api/websocket"
